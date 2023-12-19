a36c30bbd9a328bde97bf1ec63c01e56555e1049
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-19 12:35:43 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express()
-
 const PORT = 8000
+const { User } = require('./models/User')
+const { mongo_URI } = require('./config/dev')
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gyulo94:q1w2e3@boilerplate.guzruec.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(mongo_URI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
@@ -12,6 +16,21 @@ mongoose.connect('mongodb+srv://gyulo94:q1w2e3@boilerplate.guzruec.mongodb.net/?
 
 app.get('/', (req, res) => {
     res.send('안녕하세요')
+})
+
+app.post('/signup', (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body)
+    
+    const result = user.save().then(() => {
+        res.status(200).json({
+            succeess: true
+        })
+    }).catch((err) => {
+        res.json({ success: false, err })
+    })
 })
 
 app.listen(PORT, () => {
